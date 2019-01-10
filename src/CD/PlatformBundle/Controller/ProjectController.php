84039f6b3edf7710233315026fe78d5d79f93de7
Gauthier Desplanque <despla_g@modulotech.fr>
2019-01-10 18:55:49 +0100

    Ajout de la pagination pour projects_list et traductor_page

--- src/CD/PlatformBundle/Controller/ProjectController.php
+++ src/CD/PlatformBundle/Controller/ProjectController.php
@@ -24,15 +24,16 @@ class ProjectController extends Controller
     /**
      * @Route("/", name="projects_list")
      */
-	public function indexAction()
+	public function indexAction(Request $request)
 	{
 		if (null === $this->getUser()) {
-          return $this->redirectToRoute('user_login');
+      return $this->redirectToRoute('user_login');
 		}
 
 		$repository = $this->getDoctrine()->getManager()->getRepository('CDPlatformBundle:Project');
 
-		$projects = $repository->findAll();
+		$projects_list = $repository->findAll();
+    $projects = $this->get('knp_paginator')->paginate($projects_list, $request->query->get('page', 1),10);
 
 		return $this->render('CDPlatformBundle:Project:index.html.twig', array(
 			'projects' => $projects
@@ -93,7 +94,7 @@ class ProjectController extends Controller
     /**
      * @Route("/traductor", name="traductor_page")
      */
-    public function traductorViewAction()
+    public function traductorViewAction(Request $request)
     {
         if (null === $this->getUser()) {
       		return $this->redirectToRoute('projects_list');
@@ -103,7 +104,8 @@ class ProjectController extends Controller
         }
 
         $repository = $this->getDoctrine()->getManager()->getRepository('CDPlatformBundle:Project');
-		$projects = $repository->getProjectsByLangs($this->getUser()->getId(), $this->getUser()->getLangs());
+		    $projects_list = $repository->getProjectsByLangs($this->getUser()->getId(), $this->getUser()->getLangs());
+        $projects = $this->get('knp_paginator')->paginate($projects_list, $request->query->get('page', 1),10);
 
         $repository = $this->getDoctrine()->getManager()->getRepository('CDPlatformBundle:Traduction_Source');
         $random_source = $repository->getRandomUntranslatedSource($this->getUser()->getId(), $this->getUser()->getLangs());
@@ -134,7 +136,7 @@ class ProjectController extends Controller
             ->add('lang',  EntityType::class, array('class' => Lang::class, 'choice_label' => 'code', 'multiple' => false))
 			->add('save',      SubmitType::class, array('label' => 'Valider'))
 		;
-    
+
 		$form = $formBuilder->getForm();
 
 		if ($request->isMethod('POST')) {
